c71a000a87dd21717b8a271941d990d48b4d08ed
sli41 <sli41@ncsu.edu>
2016-12-02 20:00:14 -0500

    update the Rating class by adding getter and setter methods

--- model/testModule.py
+++ model/testModule.py
@@ -6,4 +6,6 @@ import numpy as np
 
 dl.test()
 ratings = dl.get_rating_data('../data/ml-latest-small/ratings_x.csv')
-print ratings.num_of_users
+print np.mean(ratings.get_rating_byItem('31'))
+print ratings.get_rating('310', '31')
+print ratings.get_item_rating_bias('31')
--- util/dataloader.py
+++ util/dataloader.py
@@ -68,11 +68,96 @@ class Ratings:
 
     # check if given userId is within the rating data set
     def check_user(self, userId):
-        return True if userId in self.users_indices else False
+        return True if userId in self.users_indices.keys() else False
 
     # check if given itemId is within the rating data set
     def check_item(self, itemId):
-        return True if itemId in self.items_indices else False
+        return True if itemId in self.items_indices.keys() else False
 
+    # calculate the number of rated entries in the rating matrix
+    def get_num_of_ratings(self):
+        check_matrix = (self.rating_matrix != self.missing_rating_default_value)
+        return np.sum(check_matrix)
 
+    # compute the sparsity of the rating matrix
+    def get_sparsity(self):
+        return 1 - self.get_num_of_ratings() / np.float64(self.num_of_users * self.num_of_items)
 
+    # compute the global average of the rating matrix (For simplicity, only consider the 0.0 default missing value)
+    def get_rating_average(self):
+        return np.sum(self.rating_matrix) / self.get_num_of_ratings()
+
+    # retrieve rating value given a (userId, itemId) pair
+    def get_rating(self, userId, itemId):
+        if self.check_user(userId) and self.check_item(itemId):
+            return self.rating_matrix[self.users_indices[userId], self.items_indices[itemId]]
+        else:
+            sys.stderr("userId or itemId is invalid!")
+            return None
+
+    # retrieve all rating values as a list given userId
+    def get_rating_byUser(self, userId):
+        if self.check_user(userId):
+            return self.rating_matrix[self.users_indices[userId], :].tolist()[0]
+        else:
+            sys.stderr("userId is invalid!")
+            return None
+
+    # retrieve all rating values as a list given itemId
+    def get_rating_byItem(self, itemId):
+        if self.check_item(itemId):
+            return self.rating_matrix[:, self.items_indices[itemId]].transpose().tolist()[0]
+        else:
+            sys.stderr("itemId is invalid!")
+            return None
+
+    # calculate the number of rated entried given a userId
+    def get_num_of_ratings_byUser(self, userId):
+        user_ratings = np.asarray(self.get_rating_byUser(userId))
+        if user_ratings is not None:
+            return np.sum(user_ratings != 0.0)
+        else:
+            sys.stderr("userId is invalid!")
+            return None
+
+    # calculate the number of rated entried given a itemId
+    def get_num_of_ratings_byItem(self, itemId):
+        item_ratings = np.asarray(self.get_rating_byItem(itemId))
+        if item_ratings is not None:
+            return np.sum(item_ratings != 0.0)
+        else:
+            sys.stderr("itemId is invalid!")
+            return None
+
+    # compute the rating bias (mean value) given a userId
+    def get_user_rating_bias(self, userId):
+        user_ratings = np.asarray(self.get_rating_byUser(userId))
+        if user_ratings is not None:
+            num_of_ratings = np.sum(user_ratings != 0.0)
+            rating_total = np.sum(user_ratings[user_ratings != 0.0])
+            rating_bias = 0 if num_of_ratings==0 else rating_total/num_of_ratings
+            return rating_bias
+        else:
+            sys.stderr("userId is invalid!")
+            return None
+
+    # compute the rating bias (mean value) given a itemId
+    def get_item_rating_bias(self, itemId):
+        item_ratings = np.asarray(self.get_rating_byItem(itemId))
+        if item_ratings is not None:
+            num_of_ratings = np.sum(item_ratings != 0.0)
+            rating_total = np.sum(item_ratings[item_ratings != 0.0])
+            rating_bias = 0 if num_of_ratings==0 else rating_total/num_of_ratings
+            return rating_bias
+        else:
+            sys.stderr("itemId is invalid!")
+            return None
+
+    # set or update the rating value for a given (userId, itemId) pair
+    def set_rating(self, userId, itemId, rating):
+        if self.check_user(userId) and self.check_item(itemId):
+            self.rating_matrix[self.users_indices[userId], self.items_indices[itemId]] = rating
+            return True
+        else:
+            sys.stderr("userId or itemId is invalid!")
+            return False
